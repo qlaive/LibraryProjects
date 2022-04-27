efa7d9b72ea43954187bb43c5342d3d358ce608b
Fee1d <Fee1d@LAPTOP-TKEA9Q46>
2022-04-27 13:59:59 -0400

    Add project files.

---
+++ LibraryProjects/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace LibraryProjects
+{
+    internal class Program
+    {
+        class Library
+        {
+            public void checkOutBook()
+            {
+                Console.Write("Enter customers name: ");
+                Console.Read();
+                Console.WriteLine("Enter book name: ");
+                Console.Read();
+
+            }
+            public void checkInBook()
+            {
+
+            }
+        }
+
+        class Books
+        {
+            public string author;
+            public string ISBN;
+            public string title;
+            public int numberOfPages;
+
+        }
+
+        class Customers
+        {
+            public string name;
+            
+
+        }
+        static void Main(string[] args)
+        {
+            Books theFellowship = new Books();
+            theFellowship.author = "J.R.R. Tolkien";
+            theFellowship.title = "Lord Of The Rings: The Fellowship Of The Ring";
+            theFellowship.numberOfPages = 423;
+
+            Customers customer1 = new Customers();
+            customer1.name = "Timothy Fee";
+
+            Customers customer2 = new Customers();
+            customer2.name = "Maribel Cerca";
+
+        }
+    }
+}
